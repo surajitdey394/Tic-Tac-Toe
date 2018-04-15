6f9d6f4846a078d69e51b93d9d7ebe96ff65f397
surajitdey394 <surajitdey394.sd@gmail.com>
2018-04-15 18:51:22 +0530

    Delete Sending_email.py

--- Sending_email.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-content="nothing"
-mail=smtplib.SMTP('smtp.gmail.com',587)
-mail.ehlo()
-mail.starttls()
-mail.login("surajitdey394.sd@gmail.com",'mahlyfmahrulz')
-mail.sendmail('surajitdey394.sd@gmail.com','surajitdey394.sd@gmail.com',content)
-mail.close()
- 
